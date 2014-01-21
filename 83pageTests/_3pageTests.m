07c8fa4205966b5c6cb0f203a48312e2c7abb756
Julie0e <julie_91@hanmail.net>
2014-01-21 13:29:36 +0900

    Initial Commit

---
+++ 83pageTests/_3pageTests.m
@@ -0,0 +1,34 @@
+//
+//  _3pageTests.m
+//  83pageTests
+//
+//  Created by 주리 on 2014. 1. 21..
+//  Copyright (c) 2014년 T. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _3pageTests : XCTestCase
+
+@end
+
+@implementation _3pageTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
